b7d829d4c2908c5bbd5d7d8f516ef8dd2d4144cb
gaurav00012 <aggarwal.gaurav611@gmail.com>
2016-10-02 17:18:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/articleName', function (req,res) {
     //articleName == article-one
     //articles[articleName]=={} content object for article one
     var articleName = req.parans.articleName;
-     res.send(createTemplate(articles[articlesName]));
+     res.send(createTemplate(articles[articleName]));
      
 });
 
