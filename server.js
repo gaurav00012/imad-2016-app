d99b361ef1d423e60bdbd0750fbd8c72e7923400
gaurav00012 <aggarwal.gaurav611@gmail.com>
2016-11-05 22:20:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,6 +105,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
+    res.send("HELLO!!!")
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
